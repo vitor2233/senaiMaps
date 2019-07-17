488727263262770290ad5e7389700ac562d21738
vitor2233 <vitorguilherme2233@gmail.com>
2019-07-16 21:09:17 -0300

    para publicar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EventsComponent } from './events/events.component';
 import { MapComponent } from './map/map.component';
+import { CampusComponent } from './campus/campus.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'eventos', component: EventsComponent },
-  { path: 'mapa', component: MapComponent }
+  { path: 'mapa', component: MapComponent },
+  { path: 'campus', component: CampusComponent }
 ];
 
 @NgModule({
-  exports: [ RouterModule ],
-  imports: [ RouterModule.forRoot(routes) ]
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule { }
